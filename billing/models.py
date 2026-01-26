74ce03f540c10c381d6d6df38852d289d0cd9c47
johnpaul <munenejohnpaul@gmail.com>
2026-01-26 05:50:20 +0300

    first commit

--- billing/models.py
+++ billing/models.py
@@ -104,6 +104,10 @@ class InvoiceItem(models.Model):
     quantity = models.PositiveIntegerField(default=1)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    @property
+    def total_price(self):
+        return self.quantity * self.unit_price
+
 class Receipt(models.Model):
     invoice = models.ForeignKey(Invoice, related_name='receipts', on_delete=models.CASCADE)
     date = models.DateField(default=timezone.now)
